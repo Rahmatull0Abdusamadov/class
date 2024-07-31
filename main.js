843319165e64d9060b55def442ad456b5fe1aa5d
Rahmatullo <rahmatulloabdusamadov167@gmail.com>
2024-07-31 09:35:46 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,75 @@
+// class task1
+        // Task1
+        // function total(a){
+            //     let x = Object.values(a)
+            //     return x.length
+            // }
+// console.log(total({ a: "moron", b: "scumbag", c:"moron", d: "dirtbag" }));
+
+//   task3
+    // let f=(a)=>{
+    //     return a.toSorted((a,b)=>a.age-b.age)
+    // }
+    // console.log(f());
+    
+
+    //  task4
+    // let f=(a,b)=>{
+    //     let str=b.split('');
+    //     for(let i=0;i<str.length;i++)a=a[str[i]];
+    //     return a;
+    // }
+    // console.log(f());
+    
+    // task5
+    // function hasKey(obj, key) {
+    //     return key in obj;
+    // }
+    // console.log(hasKey({ a: 44, b: 45, c: 46 }, "d"));
+
+    // task7
+    // function isEmpty(obj) {
+    //     return Object.keys(obj).length === 0;
+    // }
+    // console.log(isEmpty({})); 
+    // Task2
+            // function shape(a){
+                //     let obj = {
+                    //      1: "circle",
+                    //      2: "semi-circle",
+                    //      3: "triangle",
+                    //      4: "square",
+                    //      5: "pentagon",
+                    //      6: "hexagon",
+                    //      7: "heptagon",
+                    //      8: "octagon",
+//      9: "nonagon",
+//      10: "decagon"
+// }
+//     return obj[a]
+// }
+// console.log(shape(3));
+        
+    
+    // task5
+    // let f=(a)=>{
+    //     let ans=[];
+    //     for(let i=0;i<a.length;i++){
+    //         let sum=0;
+    //         for(let j=0;j<a[i].grades.length;j++){
+    //             sum+=a[i].grades[j];
+    //         }
+    //         sum/=a[i].grades.length
+    //         ans.push({name;a[i].name,grades;sum})
+    //     }
+    //     return ans
+    // }
+    // console.log();
+
+    // tassk8
+    // let f=(a,b)=>{
+    //     let ans={}; for(let i=0;i<a.length;i++) ans[a[i]]=b[i]; return ans;
+    // }
+    // console.log(f(["name", "age", "city"], ["Alice", 25, "New York"]));
+
+    
